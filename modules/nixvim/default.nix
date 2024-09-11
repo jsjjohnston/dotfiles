eb5b5bf44811960b4ab3d1bf47113aa5ee8ac4a1
Jay Johnston <jsjjohnston@gmail.com>
2024-09-11 21:17:00 +1000

    Key maps

--- modules/nixvim/default.nix
+++ modules/nixvim/default.nix
@@ -20,6 +20,111 @@
 	NERDTreeShowHidden = 1;
     };
 
+    opts = {
+	number = true;
+	relativenumber = true;
+	mouse = "a";
+	showmode = false;
+	clipboard = "unnamedplus";
+	breakindent = true;
+	undofile = true;
+	ignorecase = true;
+	smartcase = true;
+	signcolumn = "yes";
+	updatetime = 250;
+	timeoutlen = 300;
+	splitright = true;
+	splitbelow = true;
+	list = true;
+	listchars = {tab = "> "; trail = "."; nbsp ="_";}; #Fix chars
+	inccommand = "split";
+	cursorline = true;
+	scrolloff = 10;
+	hlsearch = true;
+    };
+
+    autoCmd = [
+	{
+		event = ["TextYankPost"];
+		desc = "Highlight when yanking (copying) text";
+		group = "highlight-yank"; #TODO fix yank group
+		callback = {__raw="function() vim.highlight.on_yank() end";};
+	}
+
+    ];
+
+    keymaps = [
+	{
+		mode = ["n"];
+		key = "<Esc>";
+		action = "<cmd>nohlsearch<CR>";
+		options = {desc = "Escape";};
+	}
+
+	{
+		mode = ["n"];
+		key = "[d";
+		action = "vim.diagnostic.goto_prev";
+		options = {desc="Go to previous [D]iagnostic message";};
+	}
+
+	{
+		mode = ["n"];
+		key = "]d";
+		action = "vim.diagnostic.goto_next";
+		options = {desc="Go to next [D]iagnostic message";};
+	}
+
+	{
+		mode = ["n"];
+		key = "<leader>e";
+		action = "vim.diagnostic.open_float";
+		options = {desc="Show diagnostic [E]rror messages";};
+	}
+
+	{
+		mode = ["n"];
+		key = "<leader>q";
+		action = "vim.diagnostic.setloclist";
+		options = {desc= "Open diagnostic [Q]uickfix list";};
+	}
+
+	{
+		mode = ["t"];
+		key = "<Esc><Esc>";
+		action = "<C-\\><C-n>";
+		options = {desc="Exit terminal mode";};
+	}
+
+	{
+		mode = ["n"];
+		key = "<C-h>";
+		action = "<C-w><C-h>";
+		options = {desc="Move focus to the left window";};
+	}
+
+	{
+		mode = ["n"];
+		key = "<C-l>";
+		action = "<C-w><C-l>";
+		options = {desc="Move focus to the right window";};
+	}
+
+	{
+		mode = ["n"];
+		key = "<C-j>";
+		action = "<C-w><C-j>";
+		options = {desc="Move focus to the lower window";};
+	}
+
+	{
+		mode = ["n"];
+		key = "<C-k>";
+		action = "<C-w><C-k>";
+		options = {desc="Move focus to the upper window";};
+	}
+    ];
+
     performance = {
       combinePlugins = {
         enable = true;
