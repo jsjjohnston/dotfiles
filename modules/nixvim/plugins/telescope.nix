1992181ad3045672f6b7f048bb3e28e349b67f6e
Jay Johnston <jsjjohnston@gmail.com>
2024-09-12 21:14:54 +1000

    boop

--- modules/nixvim/plugins/telescope.nix
+++ modules/nixvim/plugins/telescope.nix
@@ -1,7 +1,21 @@
 {...}: {
   programs.nixvim = {
     plugins = {
-      telescope = {enable = true;};
+      telescope = {
+        enable = true;
+        extensions = {
+          ui-select = {enable = true;};
+          fzf-native = {enable = true;};
+        };
+      };
     };
+    keymaps = [
+      {
+        mode = ["n"];
+        key = "<leader>sh";
+        action = {__raw = "require('telescope').builtin.help_tags";};
+        options = {desc = "[S]earch Help";};
+      }
+    ];
   };
 }
