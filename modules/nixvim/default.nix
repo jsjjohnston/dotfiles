9b2e40e20ff8beeede4b5a6cb85b212f2b84478a
Jay <jay@Jays-MacBook-Pro.local>
2024-10-20 15:37:12 +1100

    enabled nixvim

--- modules/nixvim/default.nix
+++ modules/nixvim/default.nix
@@ -5,20 +5,20 @@
 }: {
   imports = [
     ./plugins/neo-tree.nix
-    #./plugins/lsp.nix
-    #./plugins/colorschemes/cyberdream.nix
-    #./plugins/which-key.nix
-    #./plugins/autoformat/conform.nix
-    #./plugins/autocompletion/cmp.nix
-    #./plugins/telescope.nix
-    #./plugins/git
-    #./plugins/bufferline.nix
-    #./plugins/treesitter.nix
-    #./plugins/mini/mini-status.nix
-    #./plugins/todo-comments.nix
-   #./plugins/indent-blankline.nix
-    #./plugins/lint.nix
-    # ./plugins/airline.nix
+    ./plugins/lsp.nix
+    ./plugins/colorschemes/cyberdream.nix
+    ./plugins/which-key.nix
+    ./plugins/autoformat/conform.nix
+    ./plugins/autocompletion/cmp.nix
+    ./plugins/telescope.nix
+    ./plugins/git
+    ./plugins/bufferline.nix
+    ./plugins/treesitter.nix
+    ./plugins/mini/mini-status.nix
+    ./plugins/todo-comments.nix
+   ./plugins/indent-blankline.nix
+    ./plugins/lint.nix
+     ./plugins/airline.nix
   ];
 
   home.shellAliases.v = "nvim";
