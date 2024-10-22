1eab16ef70e4e1df680bc4e796169d819204fb18
Jay <jay@Jays-MacBook-Pro.local>
2024-10-23 09:57:42 +1100

    enable kitty

--- hosts/work-laptop/home.nix
+++ hosts/work-laptop/home.nix
@@ -1,17 +1,16 @@
 {...}: {
   imports = [
-     ../../modules/nixvim
+    ../../modules/nixvim
     # ../../modules/git
-    # ../../modules/terminals/kitty
+    ../../modules/terminals/kitty
     # ../../modules/zoxide
     # ../../modules/fzf
     # ../../modules/shell/bash
-    # ../../modules/oh-my-posh
+    ../../modules/oh-my-posh
   ];
 
   home.username = "jay";
   home.homeDirectory = "/Users/jay";
-
 
   home.stateVersion = "24.11"; # Please read the comment before changing.
 
