524db5556b46eb03e62a4b5b24d4db0008cc3de7
Jay Johnston <jay.j@montu.com.au>
2024-10-25 17:03:02 +1100

    push

--- hosts/work-laptop/configuration.nix
+++ hosts/work-laptop/configuration.nix
@@ -3,32 +3,28 @@
   pkgs,
   ...
 }: {
-  # List packages installed in system profile. To search by name, run:
-  # $ nix-env -qaP | grep wget
   environment.systemPackages = [
     pkgs.terraform
     pkgs.awscli2
     pkgs.aws-vault
     pkgs.nodejs_18
+    pkgs.bat
+    pkgs.yarn
+    pkgs.docker
+    pkgs.podman
   ];
 
   users.users.jay = {
     home = "/Users/jay";
-    #  shell = pkgs.bash;
   };
 
+  homebrew.enable = true;
   nixpkgs.config.allowUnfree = true;
 
-  # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
   nix.package = pkgs.nix;
 
-  # Create /etc/zshrc that loads the nix-darwin environment.
   programs.zsh.enable = true; # default shell on catalina
-  # programs.fish.enable = true;
-  # programs.bash.enable = true;
 
-  # Used for backwards compatibility, please read the changelog before changing.
-  # $ darwin-rebuild changelog
   system.stateVersion = 5;
 }
