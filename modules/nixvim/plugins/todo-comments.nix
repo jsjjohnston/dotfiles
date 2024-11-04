68322b8df6cc4cb78e5a9fad9d12c1754b95abb9
Jay Johnston <jay.j@montu.com.au>
2024-11-04 20:38:35 +1100

    todo comment keymaps

--- modules/nixvim/plugins/todo-comments.nix
+++ modules/nixvim/plugins/todo-comments.nix
@@ -1,5 +1,18 @@
-{...}: {
-  programs.nixvim.plugins = {
-    todo-comments.enable = true;
+{ config, lib, ... }:
+{
+  programs.nixvim.plugins.todo-comments = {
+    enable = true;
+
+    keymaps = {
+      todoTrouble.key = lib.mkIf config.programs.nixvim.plugins.trouble.enable "<leader>xq";
+      todoTelescope = lib.mkIf config.programs.nixvim.plugins.telescope.enable {
+        key = "<leader>ft";
+        keywords = [
+          "TODO"
+          "FIX"
+          "FIX"
+        ];
+      };
+    };
   };
 }
