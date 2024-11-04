e03e78cfb21e8041e8dabee267fbb209a4077b05
Jay Johnston <jay.j@montu.com.au>
2024-11-04 20:44:56 +1100

    treesitter update

--- modules/nixvim/plugins/treesitter.nix
+++ modules/nixvim/plugins/treesitter.nix
@@ -1,16 +1,61 @@
-{...}: {
-  programs.nixvim = {
-    plugins.treesitter = {
+{ config, pkgs, ... }:
+{
+  programs.nixvim.extraPlugins = [
+    # pkgs.khanelivim.tree-sitter-norg-meta
+    # pkgs.khanelivim.tree-sitter-nu
+  ];
+
+  programs.nixvim.plugins = {
+    treesitter = {
       enable = true;
+
+      folding = true;
+      grammarPackages = config.programs.nixvim.plugins.treesitter.package.passthru.allGrammars ++ [
+        # pkgs.khanelivim.tree-sitter-norg-meta
+        # pkgs.khanelivim.tree-sitter-nu
+      ];
+      nixvimInjections = true;
+
       settings = {
-        auto_install = true;
-        ensure_installed = [];
         highlight = {
+          additional_vim_regex_highlighting = true;
           enable = true;
+          disable = # Lua
+            ''
+              function(lang, bufnr)
+                return vim.api.nvim_buf_line_count(bufnr) > 10000
+              end
+            '';
         };
+
+        incremental_selection = {
+          enable = true;
+          keymaps = {
+            init_selection = "gnn";
+            node_incremental = "grn";
+            scope_incremental = "grc";
+            node_decremental = "grm";
+          };
+        };
+
         indent = {
           enable = true;
         };
+      };
+    };
+
+    treesitter-refactor = {
+      enable = true;
+
+      highlightDefinitions = {
+        enable = true;
+        clearOnCursorMove = true;
+      };
+      smartRename = {
+        enable = true;
+      };
+      navigation = {
+        enable = true;
       };
     };
   };
