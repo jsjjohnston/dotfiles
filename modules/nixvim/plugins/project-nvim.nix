68c2cb74da53a44625c9fa7f590c48c7f4745a40
Jay Johnston <jay.j@montu.com.au>
2024-11-04 20:18:52 +1100

    nix project

---
+++ modules/nixvim/plugins/project-nvim.nix
@@ -0,0 +1,24 @@
+{ config, lib, ... }:
+{
+  programs.nixvim.plugins.project-nvim = {
+    enable = true;
+    enableTelescope = true;
+  };
+
+  programs.nixvim.keymaps =
+    lib.mkIf
+      (
+        config.programs.nixvim.plugins.telescope.enable
+        && config.programs.nixvim.plugins.project-nvim.enable
+      )
+      [
+        {
+          mode = "n";
+          key = "<leader>fp";
+          action = "<cmd>Telescope projects<CR>";
+          options = {
+            desc = "Find projects";
+          };
+        }
+      ];
+}
