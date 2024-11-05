d71bb093d31d7f2f75230e4af7330141d361dc68
Jay Johnston <jay.j@montu.com.au>
2024-11-06 05:30:14 +1100

    key maps

--- modules/nixvim/keymaps.nix
+++ modules/nixvim/keymaps.nix
@@ -7,6 +7,108 @@
       key = "<esc>";
       action = "<cmd>nohc<CR>";
     }
-
+    {
+      mode = "n";
+      key = "<BS>";
+      action = "<BS>x";
+    }
+    {
+      mode = "n";
+      key = "Y";
+      action = "y$";
+      # TODO: add description
+    }
+    {
+      mode = "n";
+      key = "<C-c>";
+      action = "<cmd>b#<CR>";
+      # TODO: add description
+    }
+    {
+      mode = "n";
+      key = "<leader>[";
+      action = "<C-w>h";
+      options = {
+        desc = "Left window";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>]";
+      action = "<C-w>l";
+      options = {
+        desc = "Right window";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>,";
+      action = "<C-w>k";
+      options = {
+        desc = "Down window";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>.";
+      action = "<C-w>k";
+      options = {
+        desc = "Up window";
+      };
+    }
+    {
+      mode = "n";
+      key = "<C-[>";
+      action = "<cmd>cnext<CR>";
+      # TODO: add description
+    }
+    {
+      mode = "n";
+      key = "<C-]>";
+      action = "<cmd>cprev<CR>";
+      # TODO: add description
+    }
+    {
+      mode = "n";
+      key = "<C-Up>";
+      action = "<cmd>resize -2<CR>";
+      # TODO: add description
+    }
+    {
+      mode = "n";
+      key = "<C-Down>";
+      action = "<cmd>resize +2<CR>";
+      # TODO: add description
+    }
+    {
+      mode = "n";
+      key = "<C-Left>";
+      action = "<cmd>vertical resize +2<CR>";
+      # TODO: add description
+    }
+    {
+      mode = "n";
+      key = "<C-Right>";
+      action = "<cmd>vertical resize -2<CR>";
+      # TODO: add description
+    }
+    {
+      mode = "n";
+      key = "<M-k>";
+      action = "<cmd>move-2<CR>";
+      # TODO: add description
+    }
+    {
+      mode = "n";
+      key = "<M-j>";
+      action = "<cmd>move+<CR>";
+      # TODO: add description
+    }
+    {
+      mode = "n";
+      key = "<C-n>";
+      action = "<cmd>enew<CR>";
+      # TODO: add description
+    }
   ];
 }
