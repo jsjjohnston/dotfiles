519cbca2b8c4f0f5fddfe438ed0a2eca40d86d6c
Jay Johnston <jay.j@montu.com.au>
2025-01-07 20:11:44 +1100

    Remove lsp extra config

--- modules/nixvim/plugins/lsp.nix
+++ modules/nixvim/plugins/lsp.nix
@@ -6,44 +6,44 @@
 }:
 {
   programs.nixvim = {
-    extraConfigLuaPre = ''
-      vim.fn.sign_define("DiagnosticSignHint", { text = " 󰌵", texthl = "DiagnosticHint", linehl = "", numhl = "" })
-      vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticInfo", linehl = "", numhl = "" })
+    # extraConfigLuaPre = ''
+    #   vim.fn.sign_define("DiagnosticSignHint", { text = " 󰌵", texthl = "DiagnosticHint", linehl = "", numhl = "" })
+    #   vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticInfo", linehl = "", numhl = "" })
+    #
+    #   local function preview_location_callback(_, result)
+    #     if result == nil or vim.tbl_isempty(result) then
+    #       vim.notify('No location found to preview')
+    #       return nil
+    #     end
+    #   local buf, _ = vim.lsp.util.preview_location(result[1])
+    #     if buf then
+    #       local cur_buf = vim.api.nvim_get_current_buf()
+    #       vim.bo[buf].filetype = vim.bo[cur_buf].filetype
+    #     end
+    #   end
+    #
+    #   function peek_definition()
+    #     local params = vim.lsp.util.make_position_params()
+    #     return vim.lsp.buf_request(0, 'textDocument/definition', params, preview_location_callback)
+    #   end
+    #
+    #   local function peek_type_definition()
+    #     local params = vim.lsp.util.make_position_params()
+    #     return vim.lsp.buf_request(0, 'textDocument/typeDefinition', params, preview_location_callback)
+    #   end
+    #
+    #   require('lspconfig.ui.windows').default_options = {
+    #     border = "rounded"
+    #   }
+    # '';
 
-      local function preview_location_callback(_, result)
-        if result == nil or vim.tbl_isempty(result) then
-          vim.notify('No location found to preview')
-          return nil
-        end
-      local buf, _ = vim.lsp.util.preview_location(result[1])
-        if buf then
-          local cur_buf = vim.api.nvim_get_current_buf()
-          vim.bo[buf].filetype = vim.bo[cur_buf].filetype
-        end
-      end
-
-      function peek_definition()
-        local params = vim.lsp.util.make_position_params()
-        return vim.lsp.buf_request(0, 'textDocument/definition', params, preview_location_callback)
-      end
-
-      local function peek_type_definition()
-        local params = vim.lsp.util.make_position_params()
-        return vim.lsp.buf_request(0, 'textDocument/typeDefinition', params, preview_location_callback)
-      end
-
-      require('lspconfig.ui.windows').default_options = {
-        border = "rounded"
-      }
-    '';
-
-    autoCmd = [
-      (lib.mkIf config.programs.nixvim.plugins.lsp.servers.helm_ls.enable {
-        event = "FileType";
-        pattern = "helm";
-        command = "LspRestart";
-      })
-    ];
+    # autoCmd = [
+    #   (lib.mkIf config.programs.nixvim.plugins.lsp.servers.helm_ls.enable {
+    #     event = "FileType";
+    #     pattern = "helm";
+    #     command = "LspRestart";
+    #   })
+    # ];
   };
   programs.nixvim.plugins = {
     helm.enable = true;
@@ -208,7 +208,7 @@
           filetypes = [ "dockerfile" ];
         };
 
-        # FIX: fix exlint errors /nix/store/xwz3xij6krvkckv3ywmys7vc3zjzas7k-vscode-langservers-extracted-4.10.0/bin/vscode-eslint-language-server -v 
+        # FIX: fix exlint errors /nix/store/xwz3xij6krvkckv3ywmys7vc3zjzas7k-vscode-langservers-extracted-4.10.0/bin/vscode-eslint-language-server -v
         # eslint = {
         #   enable = true;
         #   filetypes = [
