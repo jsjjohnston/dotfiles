4d76f0fc23e93c68bce0571ad3dc187a9cbfd045
Jay Johnston <jay.j@montu.com.au>
2025-02-10 20:25:12 +1100

    nvf

--- modules/nvf/default.nix
+++ modules/nvf/default.nix
@@ -1,12 +1,49 @@
-{ ... }:
+{ config, ... }:
 {
 
   programs.nvf = {
-    enable = false;
-    # your settings need to go into the settings attribute set
-    # most settings are documented in the appendix
+    enable = !config.programs.nixvim.enable;
     settings = {
-      vim.filetree.neo-tree.enable = true;
+      vim.autocomplete.nvim-cmp = {
+      enable = true;
+      sourcePlugins = [
+        "base16"
+        "ccc"
+        "cheatsheet-nvim"
+        "cmp-buffer"
+        "cmp-luasnip"
+        "cmp-nvim-lsp"
+        "cmp-path"  
+        "cmp-treesitter"
+        "comment-nvim"
+        "friendly-snippets"
+        "fzf-lua"
+        "gitsigns-nvim"
+        "highlight-undo"
+        "hop-nvim"
+        "icon-picker-nvim"
+        "image-nvim"
+        "indent-blankline"
+        "lsp-lines"
+        "lsp-signature"
+        "lspsaga"
+        "lz-n"
+        "mini-animate"
+        
+      ];
+      };
+      vim.bell = "on";
+      vim.binds.whichKey = {
+enable = true;
+      };
+      vim.comments.comment-nvim.enable = true;
+      vim.dashboard.dashboard-nvim= {enable = true;
+      setupOpts = {};};
+      vim.filetree.nvimTree = {
+enable = true;
+mappings = { toggle = "\\"; };
+openOnSetup = false;
+      };
       vim.viAlias = true;
       vim.vimAlias = true;
       vim.lsp = {
