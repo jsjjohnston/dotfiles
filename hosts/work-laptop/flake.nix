c3baa4706d57accbac455abfc8b1fb4e2edab7da
Jay Johnston <jsjjohnston@gmail.com>
2025-03-28 10:37:33 +1100

    auto_commit_28 Mar 28 10:37:33

--- hosts/work-laptop/flake.nix
+++ hosts/work-laptop/flake.nix
@@ -19,73 +19,68 @@
     sops-nix.url = "github:Mic92/sops-nix";
   };
 
-  outputs =
-    inputs@{
-      home-manager,
-      mac-app-util,
-      nix-darwin,
-      nixpkgs,
-      nixpkgs-stable,
-      nvf,
-      self,
-      sops-nix,
-    }:
-    let
-      gitHash = _: {
-        system.configurationRevision = self.rev or self.dirtyRev or null;
-      };
-      system = "aarch64-darwin";
-
-      pkgs-stable = import nixpkgs-stable {
-        inherit system;
-        config = {
-          allowUnfree = true;
-        };
-      };
-      pkgs-unstable = import nixpkgs {
-        inherit system;
-        config = {
-          allowUnfree = true;
-        };
-      };
-    in
-    {
-      darwinConfigurations.work-laptop = nix-darwin.lib.darwinSystem {
-        specialArgs = {
-          inherit pkgs-unstable pkgs-stable inputs;
-        };
-        modules = [
-          ./configuration.nix
-          gitHash
-          mac-app-util.darwinModules.default
-          home-manager.darwinModules.home-manager
-          {
-            users.users.jay.home = "/Users/jay";
-            home-manager = {
-              useGlobalPkgs = false;
-              useUserPackages = true;
-              extraSpecialArgs = {
-                inherit pkgs-stable pkgs-unstable inputs;
-              };
-              sharedModules = [
-                sops-nix.homeManagerModules.sops
-
-                nvf.homeManagerModules.default
-
-              ];
-              users = {
-                jay = {
-                  imports = [
-                    mac-app-util.homeManagerModules.default
-                    ./home.nix
-                  ];
-                };
-              };
-              backupFileExtension = "backup";
-            };
-          }
-        ];
-      };
-
+  outputs = inputs @ {
+    home-manager,
+    mac-app-util,
+    nix-darwin,
+    nixpkgs,
+    nixpkgs-stable,
+    nvf,
+    self,
+    sops-nix,
+  }: let
+    gitHash = _: {
+      system.configurationRevision = self.rev or self.dirtyRev or null;
     };
+    system = "aarch64-darwin";
+
+    pkgs-stable = import nixpkgs-stable {
+      inherit system;
+      config = {
+        allowUnfree = true;
+      };
+    };
+    pkgs-unstable = import nixpkgs {
+      inherit system;
+      config = {
+        allowUnfree = true;
+      };
+    };
+  in {
+    darwinConfigurations.work-laptop = nix-darwin.lib.darwinSystem {
+      specialArgs = {
+        inherit pkgs-unstable pkgs-stable inputs;
+      };
+      modules = [
+        ./configuration.nix
+        gitHash
+        mac-app-util.darwinModules.default
+        home-manager.darwinModules.home-manager
+        {
+          users.users.jay.home = "/Users/jay";
+          home-manager = {
+            useGlobalPkgs = false;
+            useUserPackages = true;
+            extraSpecialArgs = {
+              inherit pkgs-stable pkgs-unstable inputs;
+            };
+            sharedModules = [
+              sops-nix.homeManagerModules.sops
+
+              nvf.homeManagerModules.default
+            ];
+            users = {
+              jay = {
+                imports = [
+                  mac-app-util.homeManagerModules.default
+                  ./home.nix
+                ];
+              };
+            };
+            backupFileExtension = "backup";
+          };
+        }
+      ];
+    };
+  };
 }
