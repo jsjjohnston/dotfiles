e6e8d45cc90be0fd2a0a2274579936c4769fb19a
Jay Johnston <jsjjohnston@gmail.com>
2025-06-11 09:01:57 +1000

    auto_commit_11 Jun 11 09:01:57

--- modules/aerospace/default.nix
+++ modules/aerospace/default.nix
@@ -1,32 +1,10 @@
 {
   programs.sketchybar = {
     enable = true;
-    config = ''
-      # Define colors
-      export COLOR_BLACK="0xff181926"
-      export COLOR_WHITE="0xffcad3f5"
-
-      # Configure bar
-      sketchybar --bar height=32 \
-                      position=top \
-                      padding_left=10 \
-                      padding_right=10 \
-                      color=$COLOR_BLACK
-
-      # Configure default values
-      sketchybar --default icon.font="SF Pro:Bold:14.0" \
-                          icon.color=$COLOR_WHITE \
-                          label.font="SF Pro:Bold:14.0" \
-                          label.color=$COLOR_WHITE
-
-      # Add items to the bar
-      sketchybar --add item clock right \
-                --set clock script="date '+%H:%M'" \
-                            update_freq=10
-
-      # Update the bar
-      sketchybar --update
-    '';
+    config = {
+      source = ./config;
+      recursive = true;
+    };
   };
   programs.aerospace = {
     enable = true;
@@ -169,7 +147,7 @@
         outer = {
           left = 0;
           bottom = 0;
-          top = 0;
+          top = 32;
           right = 0;
         };
       };
