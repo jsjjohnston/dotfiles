20ee985b393e3299fc551af1b3074d6bb27a2c3c
Jay Johnston <jsjjohnston@gmail.com>
2025-07-03 16:28:14 +1000

    set cert only on mac

--- configuration/substituters/default.nix
+++ configuration/substituters/default.nix
@@ -1,22 +1,31 @@
-{config, ...}: {
-  nix.settings = {
-    ssl-cert-file = "/Library/Application Support/Netskope/netskope-cert-bundle.pem";
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: {
+  nix.settings = lib.mkMerge [
+    (lib.mkIf pkgs.stdenv.isDarwin {
+      ssl-cert-file = "/Library/Application Support/Netskope/netskope-cert-bundle.pem";
+    })
 
-    trusted-public-keys = [
-      "localhost:ZQiZIzpEmLe22JKUhV8sj02d5mq7HK5UVZXVWDlvhgc="
-      "ncps.aldarow.dev:/yu7Apir5mzO8tcb/h8y3ySUSrWPCCVEG78EzTKkmWU="
-    ];
-    substituters =
-      if config.networking.hostName == "jay-server"
-      then [
-        "http://localhost:5000/"
-      ]
-      else [
-        "https://ncps.aldarow.dev/"
+    {
+      trusted-public-keys = [
+        "localhost:ZQiZIzpEmLe22JKUhV8sj02d5mq7HK5UVZXVWDlvhgc="
+        "ncps.aldarow.dev:/yu7Apir5mzO8tcb/h8y3ySUSrWPCCVEG78EzTKkmWU="
       ];
-    trusted-substituters = [
-      "https://ncps.aldarow.dev/"
-      "http://localhost:5000/"
-    ];
-  };
+      substituters =
+        if config.networking.hostName == "jay-server"
+        then [
+          "http://localhost:5000/"
+        ]
+        else [
+          "https://ncps.aldarow.dev/"
+        ];
+      trusted-substituters = [
+        "https://ncps.aldarow.dev/"
+        "http://localhost:5000/"
+      ];
+    }
+  ];
 }
