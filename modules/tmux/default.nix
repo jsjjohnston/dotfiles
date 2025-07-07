34b5f428a62b262cffaa061394b5d3b5a9100fa4
Jay Johnston <jsjjohnston@gmail.com>
2025-07-08 09:43:25 +1000

    sesh config

--- modules/tmux/default.nix
+++ modules/tmux/default.nix
@@ -30,6 +30,8 @@
       bind '"' split-window -c "#{pane_current_path}"
       bind % split-window -h -c "#{pane_current_path}"
       set-option -g allow-rename off
+      set -g detach-on-destroy off
+      bind-key x kill-pane
     '';
     plugins = with pkgs; [
       {
